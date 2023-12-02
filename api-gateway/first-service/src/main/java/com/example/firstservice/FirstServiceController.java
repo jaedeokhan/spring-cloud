391a6580161388562e37fc949584cae3c6659da2
jaedeokhan <hjaedeok@gmail.com>
2023-12-02 14:31:56 +0900

    feat : Spring Cloud Gateway - Filter Java Config

--- api-gateway/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ api-gateway/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,9 +1,14 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
@@ -11,5 +16,11 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info("first-request : {}", header);
+        return "Hello World In First Service.";
     }
 }
