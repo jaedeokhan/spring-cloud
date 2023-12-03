d85cf55f9eb5c2616ff9a78365c13d8ae73d5949
jaedeokhan <hjaedeok@gmail.com>
2023-12-03 13:54:29 +0900

    feat : Spring Cloud Gateway - Load Balancer random port

--- api-gateway/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ api-gateway/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,16 +1,23 @@
 package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
+    Environment env;
 
+    public SecondServiceController(Environment env) {
+        this.env = env;
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service.";
@@ -23,7 +30,9 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from Second Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server Port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from Second Service on PORT %s"
+                , env.getProperty("local.server.port"));
     }
 }
