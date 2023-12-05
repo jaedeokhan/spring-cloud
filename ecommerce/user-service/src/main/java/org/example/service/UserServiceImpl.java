49a9c9389903011d331081142b6bd8a03bb97619
jaedeokhan <hjaedeok@gmail.com>
2023-12-05 23:55:53 +0900

    feat : Users Microservice - Spring Security 연동 및 Eureka Server Application 사라지지 않는 버그

--- ecommerce/user-service/src/main/java/org/example/service/UserServiceImpl.java
+++ ecommerce/user-service/src/main/java/org/example/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.example.dto.UserDto;
 import org.example.jpa.UserEntity;
 import org.example.jpa.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,12 +16,14 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper mapper;
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
 
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPassword("encrypted_password");
+        userEntity.setEncryptedPassword(passwordEncoder.encode(userDto.getPassword()));
 
         userRepository.save(userEntity);
 
